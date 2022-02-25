065dc1259ea633f005a51812601750af36603d8b
Esra Yazar <esrayzr@gmail.com>
2022-02-25 18:28:20 -0500

    Fixed the countryMeal type

--- src/main/java/com/coding/meal_kit/models/CountryMeals.java
+++ src/main/java/com/coding/meal_kit/models/CountryMeals.java
@@ -2,32 +2,12 @@ package com.coding.meal_kit.models;
 
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import java.util.List;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 
-@Entity
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Table(name="details")
 public class CountryMeals {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	private String strMeal;
-	private String strMealThumb;
-	private String idMeal;
-
-	
+	List<CountryMeal> meals;
 }
